208ced6f502d220d3917a6fd55f58901df16db36
tingyou.lai@happy-seed.com <lty5201314>
2021-04-19 17:05:23 +0800

    feat: add jest

---
+++ test/auth.spec.js
@@ -0,0 +1,11 @@
+const app = require('../app')
+describe('test cli', () => {
+    beforeAll(() => {
+    })
+    test('is foo', () => {
+    })
+    test('is not bar', () => {
+    })
+    afterAll(() => {
+    })
+})
